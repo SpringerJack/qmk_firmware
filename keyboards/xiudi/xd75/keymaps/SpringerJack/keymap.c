8a2e8b4b5d706cef631403a0d1756f8ba5fd832b
K0kytos <k0kytos@pm.me>
2024-01-30 16:23:56 +0100

    Shift pfusch

--- keyboards/xiudi/xd75/keymaps/SpringerJack/keymap.c
+++ keyboards/xiudi/xd75/keymaps/SpringerJack/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_NO,         KC_NO,    KC_NO,      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
                             KC_TAB , KC_NO,   KC_W,    KC_E,    KC_R,    KC_T,   KC_VOLD,       KC_MUTE,  KC_VOLU,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
                             KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   OSM(MOD_LSFT), KC_NO,    C(KC_BSPC), KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-                            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_MPLY,       KC_MPRV,  KC_MNXT,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                            KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_MPLY,       KC_MPRV,  KC_MNXT,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
                             KC_NO,   KC_Q,    KC_NO,   KC_LSFT, MO(_NN), KC_SPC, OS_CMOD,       FN,       OS_CMOD,    KC_ENT, MO(_NN), KC_RSFT, KC_NO,   KC_RBRC, KC_NO),
 
   [_CMOD] = LAYOUT_ortho_5x15(
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                             KC_NO, KC_PGUP, KC_BSPC, KC_UP, KC_DELETE, KC_PGDN, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_MINS,
                             KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_MINS, KC_NO,
-                            KC_LSFT, KC_ESC, KC_PASTE, KC_TAB, KC_UNDO, KC_AGAIN, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_TRNS, KC_RSFT,
+                            KC_TRNS,KC_ESC, KC_PASTE, KC_TAB, KC_UNDO, KC_AGAIN, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
   /* Below is infrequently used */
