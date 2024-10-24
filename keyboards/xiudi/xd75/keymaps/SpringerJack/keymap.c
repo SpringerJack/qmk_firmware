602ab926e4a04b6fbc4cf6020b1abd0f4377098c
K0kytos <k0kytos@pm.me>
2024-10-24 11:03:27 +0200

    I do not have this keyboard

--- keyboards/xiudi/xd75/keymaps/SpringerJack/keymap.c
+++ keyboards/xiudi/xd75/keymaps/SpringerJack/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DE] = LAYOUT_ortho_5x15(
                             KC_NO,   KC_1,    KC_2,  KC_3,    KC_4,    KC_5,   KC_NO,     KC_NO,   KC_NO,      KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_NO,
                             KC_TAB , KC_Q,    KC_W,  KC_E,    KC_R,    KC_T,   KC_VOLD,   KC_MUTE, KC_VOLU,    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
-                            KC_CAPS, KC_A,    KC_S,  KC_D,    KC_F,    KC_G,   KC_F13,    FN,      C(KC_BSPC), KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+                            KC_CAPS, KC_A,    KC_S,  KC_D,    KC_F,    KC_G,   OSL(_FN),  FN,      C(KC_BSPC), KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
                             KC_LGUI, KC_Z,    KC_X,  KC_C,    KC_V,    KC_B,   KC_MPLY,   KC_MPRV, KC_MNXT,    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LGUI,
                             KC_NO,   KC_LGUI, KC_NO, KC_LSFT, MO(_NN), KC_SPC, KC_LCTL,   KC_LALT, KC_RCTL,    KC_ENT, MO(_NN), KC_RSFT, KC_NO,  KC_RBRC, KC_NO),
 
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_NO,   KC_PGUP, KC_BSPC,  KC_UP,   KC_DELETE, KC_PGDN,  KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS, KC_7,  KC_8,    KC_9,    KC_NO,   KC_MINS,
                             KC_TAB,  KC_HOME, KC_LEFT,  KC_DOWN, KC_RIGHT,  KC_END,   KC_TRNS, KC_TRNS, KC_NO,   KC_MINS, KC_4,  KC_5,    KC_6,    KC_COMM, KC_TAB,
                             KC_TRNS, KC_UNDO, KC_AGAIN, KC_TAB,  KC_ESC,    KC_PASTE, KC_TRNS, KC_TRNS, KC_NO,   KC_PLUS, KC_1,  KC_2,    KC_3,    KC_DOT,  KC_TRNS,
-                            KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                            KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_ENT,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
   [_FN] = LAYOUT_ortho_5x15(
                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
